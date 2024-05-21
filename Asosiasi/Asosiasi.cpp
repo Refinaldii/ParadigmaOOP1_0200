a54b17d00d66acb8265e937f043714539d09b634
Refinaldii <refinaldi9786@gmail.com>
2024-05-21 12:57:21 +0700

    membuat class dokter lingkup public

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -17,6 +17,19 @@ public:
     void tambahDokter(dokter*);
     void cetakDokter();
 };
+
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+};
 int main()
 {
     std::cout << "Hello World!\n";
